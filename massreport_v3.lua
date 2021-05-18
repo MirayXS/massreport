90d89fb2a50c55a2987fb516d49d32473ce33c70
Miray <cinziapansini1973@gmail.com>
2021-05-19 01:55:52 +0200

    Create massreport_v3.lua

---
+++ massreport_v3.lua
@@ -0,0 +1,346 @@
+function RandomText()
+    MyTable = {}
+    Text = {}
+    function NoCAPS()
+        Random = math.random(1,26)
+        if Random == 1 then 
+            MyTable[Random] = "a"
+        end
+        if Random == 2 then 
+            MyTable[Random] = "b"
+        end
+        if Random == 3 then
+            MyTable[Random] = "c"
+        end
+        if Random == 4 then
+            MyTable[Random] = "d"
+        end
+        if Random == 5 then
+            MyTable[Random] = "e"
+        end
+        if Random == 6 then
+            MyTable[Random] = "f"
+        end
+        if Random == 7 then
+            MyTable[Random] = "g"
+        end
+        if Random == 8 then
+            MyTable[Random] = "h"
+        end
+        if Random == 9 then
+            MyTable[Random] = "i"
+        end
+        if Random == 10 then
+            MyTable[Random] = "j"
+        end
+        if Random == 11 then
+            MyTable[Random] = "k"
+        end
+        if Random == 12 then
+            MyTable[Random] = "l"
+        end
+        if Random == 13 then
+            MyTable[Random] = "m"
+        end
+        if Random == 14 then
+            MyTable[Random] = "n"
+        end
+        if Random == 15 then
+            MyTable[Random] = "o"
+        end
+        if Random == 16 then
+            MyTable[Random] = "p"
+        end
+        if Random == 17 then
+            MyTable[Random] = "q"
+        end
+        if Random == 18 then
+            MyTable[Random] = "r"
+        end
+        if Random == 19 then
+            MyTable[Random] = "s"
+        end
+        if Random == 20 then
+            MyTable[Random] = "t"
+        end
+        if Random == 21 then
+            MyTable[Random] = "u"
+        end
+        if Random == 22 then
+            MyTable[Random] = "v"
+        end
+        if Random == 23 then
+            MyTable[Random] = "w"
+        end
+        if Random == 24 then
+            MyTable[Random] = "x"
+        end
+        if Random == 25 then
+            MyTable[Random] = "y"
+        end
+        if Random == 26 then
+            MyTable[Random] = "z"
+        end
+    end
+    
+    function CAPS()
+        Random = math.random(1,26)
+        if Random == 1 then 
+            MyTable[Random] = "A"
+        end
+        if Random == 2 then 
+            MyTable[Random] = "B"
+        end
+        if Random == 3 then
+            MyTable[Random] = "C"
+        end
+        if Random == 4 then
+            MyTable[Random] = "D"
+        end
+        if Random == 5 then
+            MyTable[Random] = "E"
+        end
+        if Random == 6 then
+            MyTable[Random] = "F"
+        end
+        if Random == 7 then
+            MyTable[Random] = "G"
+        end
+        if Random == 8 then
+            MyTable[Random] = "H"
+        end
+        if Random == 9 then
+            MyTable[Random] = "I"
+        end
+        if Random == 10 then
+            MyTable[Random] = "J"
+        end
+        if Random == 11 then
+            MyTable[Random] = "K"
+        end
+        if Random == 12 then
+            MyTable[Random] = "L"
+        end
+        if Random == 13 then
+            MyTable[Random] = "M"
+        end
+        if Random == 14 then
+            MyTable[Random] = "N"
+        end
+        if Random == 15 then
+            MyTable[Random] = "O"
+        end
+        if Random == 16 then
+            MyTable[Random] = "P"
+        end
+        if Random == 17 then
+            MyTable[Random] = "Q"
+        end
+        if Random == 18 then
+            MyTable[Random] = "R"
+        end
+        if Random == 19 then
+            MyTable[Random] = "S"
+        end
+        if Random == 20 then
+            MyTable[Random] = "T"
+        end
+        if Random == 21 then
+            MyTable[Random] = "U"
+        end
+        if Random == 22 then
+            MyTable[Random] = "V"
+        end
+        if Random == 23 then
+            MyTable[Random] = "W"
+        end
+        if Random == 24 then
+            MyTable[Random] = "X"
+        end
+        if Random == 25 then
+            MyTable[Random] = "Y"
+        end
+        if Random == 26 then
+            MyTable[Random] = "Z"
+        end
+    end
+    number = 0
+    function add()
+        number = number + 1
+    end
+    function RandomCaps()
+        Random = math.random(1,2)
+        if Random == 1 then
+            NoCAPS()
+        end
+        if Random == 2 then
+            CAPS()
+        end
+    end
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps();add()
+    Text[number] = MyTable[Random]
+    RandomCaps();add()
+    Text[number] = MyTable[Random]
+    RandomCaps();add()
+    Text[number] = MyTable[Random]
+    RandomCaps();add()
+    Text[number] = MyTable[Random]
+    RandomCaps();add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+    RandomCaps()
+    add()
+    Text[number] = MyTable[Random]
+end
+game:GetService('RunService').Stepped:connect(function()
+    RandomText()
+    Report = 
+    Text[1]..
+    Text[2]..
+    Text[3]..
+    Text[4]..
+    Text[5]..
+    Text[6]..
+    Text[7]..
+    Text[8]..
+    Text[9]..
+    Text[10]..
+    Text[11]..
+    Text[12]..
+    Text[13]..
+    Text[14]..
+    Text[15]..
+    Text[16]..
+    Text[17]..
+    Text[18]..
+    Text[19]..
+    Text[20]..
+    Text[21]..
+    Text[22]..
+    Text[23]..
+    Text[24]..
+    Text[25]..
+    Text[26]..
+    Text[27]..
+    Text[28]..
+    Text[29]..
+    Text[30]..
+    Text[31]..
+    Text[32]..
+    Text[33]..
+    Text[34]..
+    Text[35]..
+    Text[36]..
+    Text[37]..
+    Text[38]..
+    Text[39]..
+    Text[40]
+    For = "Exploiting"
+    for i,v in pairs(game.Players:GetPlayers()) do
+        if v.Name ~= game.Players.LocalPlayer.Name then
+            game.Players:ReportAbuse(v, For, Report)
+            print(v.Name.." Reported For "..Report)
+        end
+    end
+end)
