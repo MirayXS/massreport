7cdade47d6bd1a1bf02ee811e855bd077ada80e8
Miray <cinziapansini1973@gmail.com>
2021-05-19 01:55:09 +0200

    Create massreport_fixed.lua

---
+++ massreport_fixed.lua
@@ -0,0 +1,3 @@
+while wait() do
+game.Players:ReportAbuse(game.Players.LocalPlayer, "Exploiting", "User was recently IP banned and terminated from roblox for selling robux offsite, he now has bypassed the IP ban with proxy and made a account to break the tos")
+end
